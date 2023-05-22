1461cdfcf5d7afddaf2b66f49d6c9a8b75d5adc7
leocsar <leoccet@gmail.com>
2023-05-22 00:49:06 -0300

    Projeto finalizado

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', '192.168.0.46'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
